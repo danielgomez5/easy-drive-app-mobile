b197faafc627e90d34ae206932cb7611d19380e8
dawid <dawidrafal_eibin@iescarlesvallbona.cat>
2025-03-27 19:25:46 +0100

    Splash activity (logo al encendre la app) s'ha acabat

--- app/src/main/java/com/example/easydrive/activities/MainActivity.kt
+++ app/src/main/java/com/example/easydrive/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.easydrive
+package com.example.easydrive.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.easydrive.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
 
     }
 }
---
+++ app/src/main/java/com/example/easydrive/activities/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.easydrive.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.easydrive.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler().postDelayed(Runnable { //This method will be executed once the timer is over
+            // Start your app main activity
+            val i = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(i)
+            // close this activity
+            finish()
+        }, 2000)
+        /*startActivity(Intent(this, MainActivity::class.java))
+        finish()*/
+    }
+}
